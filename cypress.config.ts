aa0fa0be0ace37a7c836cc7a7dca134929bbe403
Tim Veletta <tim.veletta@mechanicalrock.io>
2022-07-19 17:25:07 +0800

    build: adding empty env to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000/',
+		env: {
+			AZURE_CLIENT_ID: '',
+			AZURE_TENANT_ID: '',
+			AZURE_CLIENT_SECRET: '',
+			USERNAME: '',
+			PASSWORD: '',
+		},
 	},
 });
