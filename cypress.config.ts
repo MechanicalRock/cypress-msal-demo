7c4ab3f3d23f093a0777a46cf42b08b8b2adcb5f
Tim Veletta <tim.veletta@mechanicalrock.io>
2022-07-19 17:19:53 +0800

    chore: including cypress.config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,12 +3,5 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000/',
-		env: {
-			AZURE_CLIENT_ID: '',
-			AZURE_TENANT_ID: '',
-			AZURE_CLIENT_SECRET: '',
-			USERNAME: 'username',
-			PASSWORD: 'password1234',
-		},
 	},
 });
